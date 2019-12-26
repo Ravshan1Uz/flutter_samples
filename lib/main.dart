a4b760907b29dc18e329960ac24c6cc3248672dd
ravshan <ravshanjavacoder@gmail.com>
2019-12-26 19:49:39 +0500

    first lesson

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,21 @@ void main() => runApp(MaterialApp(
         centerTitle: true,
       ),
       body: Center(
-        child: Text("Hello ninjas!"),
+        child: Text(
+          "Hello ninjas!",
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'Aladin',
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
+        onPressed: () {},
         child: Text('click'),
+        backgroundColor: Colors.red[600],
       ),
     )
   ),
