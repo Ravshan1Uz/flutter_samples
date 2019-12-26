f57fb6ac7425dbd3d10fb7f633ba7bd84c216a80
ravshan <ravshanjavacoder@gmail.com>
2019-12-26 19:55:49 +0500

    first lesson

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-    home: Scaffold(
+    home: Home()
+  ),
+);
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text("My First App"),
         centerTitle: true,
+        backgroundColor: Colors.red[600],
       ),
       body: Center(
         child: Text(
@@ -23,6 +31,7 @@ void main() => runApp(MaterialApp(
         child: Text('click'),
         backgroundColor: Colors.red[600],
       ),
-    )
-  ),
-);
+    );
+  }
+
+}
