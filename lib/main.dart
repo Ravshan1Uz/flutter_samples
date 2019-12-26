bf72c7a7a213421f3c40e72d6af8a84725d29a95
ravshan <ravshanjavacoder@gmail.com>
2019-12-26 19:41:41 +0500

    first lesson

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Text("Hey ninjas!")));
+void main() => runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("My First App"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text("Hello ninjas!"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('click'),
+      ),
+    )
+  ),
+);
